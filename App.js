651228c6f5ad1476c20c71e991594f1af586bf48
Leonardo de Lima Junior <juhhvoid@gmail.com>
2021-03-18 14:54:15 -0300

    first commit

---
+++ App.js
@@ -0,0 +1,109 @@
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, Image, TouchableOpacity} from 'react-native';
+
+import Slider from '@react-native-community/slider';
+import Clipboard from 'expo-clipboard';
+
+let charset = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+
+export default function App(){
+  const [password, setPassword] = useState('');
+  const [size, setSize] = useState(5);
+
+
+  function generatePass(){
+    let pass = '';
+    for(let i = 0, n = charset.length; i < size; i++){
+      pass += charset.charAt(Math.floor(Math.random() * n))
+    }
+    setPassword(pass);
+  }
+
+  function copyPass(){
+    Clipboard.setString(password);
+    alert('Senha copiada!');
+  }
+
+  return(
+    <View style={styles.container}>
+
+      <View style={styles.container}>
+        <Text style={styles.title}>Password Generator</Text>
+      </View>
+
+      <Image
+        source={require('./src/assets/logo.png')}
+        style={styles.logo}
+      />
+
+      <Text style={styles.title}> {size} Characters</Text>
+
+      <View style={styles.area}>
+        <Slider
+          style={{ height: 50 }}
+          minimumValue={5}
+          maximumValue={15}
+          minimumTrackTintColor="#FF0000"
+          maximumTrackTintColor="#000"
+          value={size}
+          onValueChange={ (valor) => setSize(valor.toFixed(0))}
+        />
+      </View>
+
+      <TouchableOpacity style={styles.button} onPress={generatePass} >
+          <Text style={styles.buttonText}>Generate</Text>
+      </TouchableOpacity>
+
+
+      {password !== '' && (
+        <View style={styles.area}>
+        <Text style={styles.password} onLongPress={copyPass}> {password} </Text>
+      </View>
+      )}
+      
+
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container:{
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#f3f3f3'
+  },
+  logo:{
+    marginBottom: 60
+  },
+  title:{
+    fontSize: 30,
+    fontWeight: 'bold'
+  },
+  area:{
+    marginTop: 15,
+    marginBottom: 15,
+    backgroundColor: '#fff',
+    width: '90%',
+    borderRadius: 7
+  },
+  button:{
+    backgroundColor: '#FFA200',
+    width: '80%',
+    height: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 7,
+    marginBottom: 25
+  },
+  buttonText:{
+    fontSize: 20,
+    color: '#FFF',
+    fontWeight: 'bold'
+  },
+  password:{
+    padding: 10,
+    textAlign: 'center',
+    fontSize: 20
+  }
+});
